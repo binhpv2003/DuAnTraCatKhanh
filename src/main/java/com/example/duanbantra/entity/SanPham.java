6bc92a30ab2457da4d573c1c95112869edbee03d
Pham Van Binh <binhpvph29510>
2023-10-09 14:01:57 +0700

    Changes

--- src/main/java/com/example/duanbantra/entity/SanPham.java
+++ src/main/java/com/example/duanbantra/entity/SanPham.java
@@ -1,4 +1,38 @@
 package com.example.duanbantra.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "san_pham")
 public class SanPham {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "HanSD")
+    private int hanSD;
+
+    @Column(name = "hinhAnh")
+    private String hinhAnh;
+
+    @Column(name = "moTa")
+    private String moTa;
+
+    @Column(name = "SoLuongTon")
+    private int soLuong;
+
+    @Column(name = "GiaNhap")
+    private Long giaNhap;
+
+    @Column(name = "GiaBan")
+    private Long giaBan;
+
+    @Main
+
 }
